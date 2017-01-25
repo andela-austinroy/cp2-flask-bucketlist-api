167c42474dcf4f745d83b7223c345b82c71f1d2a
Austin Roy <austin.roy@andela.com>
2017-01-25 18:57:05 +0300

    [Feature] added pagination and name querying

--- app/bucketlists/views.py
+++ app/bucketlists/views.py
@@ -14,36 +14,40 @@ bucketlists = Blueprint('bucketlists', __name__, url_prefix='/bucketlists')
 
 buckets = []
 
+
 @app.errorhandler(401)
 def custom401error(exception):
     return jsonify(exception.description), 401
+
 
 @app.errorhandler(403)
 def custom403error(exception):
     return jsonify(exception.description), 403
 
+
 @app.errorhandler(404)
 def custom404error(exception):
     return jsonify(exception.description), 404
 
+
 @app.errorhandler(400)
 def custom400error(exception):
     return jsonify(exception.description), 400
+
 
 @auth.verify_token
 def verify_auth_token(token):
     s = Serializer(app.config['SECRET_KEY'])
     try:
         data = s.loads(token)
-        user = User.query.filter_by(username=data['username'])
+        user = User.query.filter_by(username=data['username']).scalar()
         if user:
-            g.user = user
+            g.user_id = user.id
             return True
-    except (SignatureExpired ,BadSignature):
-        return abort(401, {'message':'invalid token'})  # invalid token
+    except (SignatureExpired, BadSignature):
+        return abort(401, {'message': 'invalid token'})
 
     return False
-    
 
 
 @app.route('/')
@@ -72,23 +76,54 @@ def create_new_bucketlist():
 @auth.login_required
 def fetch_all_bucketlists():
     """Returns all bucketlists"""
-    bucketlists = BucketList.query.all()
-    for bucketlist in bucketlists:
-        items = BucketListItem.query.filter_by(
+
+    page_no = request.args.get('pag_no', 1)
+    limit = request.args.get('limit', 20)
+    q_name = request.args.get('q', "")
+
+    bucketlists = BucketList.query.filter_by(
+        created_by=g.user_id).filter(
+        BucketList.name.like('%{}%.'.format(q_name))).paginate(
+        int(page_no), int(limit))
+
+    if not bucketlists:
+        abort(404, {"error": "No bucketlists added"})
+    for bucketlist in bucketlists.items:
+        bucket_items = BucketListItem.query.filter_by(
             bucketlist_id=bucketlist.id).first()
-        if items is None:
-            items = []
+        if bucket_items is None:
+            bucket_items = []
         else:
-            items = items.name
-        buckets.append({'id': bucketlist.id,
-                        'name': bucketlist.name,
-                        'items': items,
-                        'date_created': bucketlist.date_created,
-                        'date_modified': bucketlist.date_modified
-                        })
-    if not len(buckets):
-        return jsonify({"error": "No bucketlists added"}), 404
-    return jsonify({'bucketlists': buckets}), 200
+            list_items = [bucket_item.name for bucket_item in bucket_items]
+        # buckets.append({'id': bucketlist.id,
+        #                 'name': bucketlist.name,
+        #                 'items': bucket_items,
+        #                 'date_created': bucketlist.date_created,
+        #                 'date_modified': bucketlist.date_modified
+        #                 })
+    return jsonify({
+        'Bucketlist': [
+            {
+                'id': bucketlist.id,
+                'name': bucketlist.name,
+                'created_by': bucketlist.created_by,
+                'items': list_items,
+                'date_created': bucketlist.date_created,
+                'date_modified': bucketlist.date_modified
+            } for bucketlist in bucketlists.items
+
+        ],
+        'next': '{}?q={}&limit={}&page={}'.format(
+            str(url_for('bucketlists.get_bucketlists', _external=True)),
+            str(q_name),
+            str(limit),
+            str(page_no + 1)) if bucketlists.has_next else None,
+        'prev': '{}?q={}&limit={}&page={}'.format(
+            str(url_for('bucketlists.get_bucketlists', _external=True)),
+            str(q_name),
+            str(limit),
+            str(page_no - 1)) if bucketlists.has_prev else None
+    }), 200
 
 
 @app.route('/bucketlists/?q=Check', methods=['GET'])
@@ -127,14 +162,16 @@ def update_bucketlist(id):
         id=id).scalar()
     if not update_bucket:
         return jsonify({"error": "Bucketlist not found"}), 404
-    existing_bucketlist = BucketList.query.filter_by(name=request.form.get('name')).first()
+    existing_bucketlist = BucketList.query.filter_by(
+        name=request.form.get('name')).first()
     if existing_bucketlist:
-        return abort(403, {'error':'That bucketlist name has already been used'})
+        return abort(403,
+                     {'error': 'That bucketlist name has already been used'})
     update_bucket.name = request.form.get('name', update_bucket.name)
     update_bucket.save()
     return jsonify({"success": "Changes saved to bucketlist",
-        "name": update_bucket.name
-        }), 200
+                    "name": update_bucket.name
+                    }), 200
 
 
 @app.route('/bucketlists/<id>', methods=['DELETE'])
@@ -155,7 +192,7 @@ def add_bucketlist_item(id):
     bucketlist_id = request.form.get('id')
     bucketlist = BucketList.query.filter_by(id=id).first()
     if not bucketlist:
-        return abort(404, {"error" : "bucket list does not exist"})
+        return abort(404, {"error": "bucket list does not exist"})
     name = request.form.get('name')
     description = request.form.get('description')
     done = 'false'
@@ -180,7 +217,7 @@ def update_bucketlist_item(id, item_id):
     update_bucket_item = BucketListItem.query.filter_by(
         id=item_id, bucketlist_id=id).scalar()
     if not update_bucket_item:
-        abort(404, ({"error" : "buckelist item not found"}))
+        abort(404, ({"error": "buckelist item not found"}))
     if request.form.get('name'):
         update_bucket_item.name = request.form.get('name')
     if request.form.get('description'):
@@ -188,18 +225,20 @@ def update_bucketlist_item(id, item_id):
             'description', update_bucket_item.description)
     if request.form.get('done'):
         print request.form.get('done')
-        if request.form.get('done').upper() != "TRUE" and request.form.get('done').upper() != "FALSE":
-            return abort(400, {"error": "done should be either 'True' or 'False'"})
+        if request.form.get('done').upper() != "TRUE" \
+                and request.form.get('done').upper() != "FALSE":
+            return abort(400,
+                         {"error": "done should be either 'True' or 'False'"})
         if request.form.get('done').upper() == "TRUE":
             update_bucket_item.done = True
         elif request.form.get('done').upper() == "FALSE":
             update_bucket_item.done = False
     update_bucket_item.save()
     return jsonify({"success": "Changes saved to item",
-        "name": update_bucket_item.name,
-        "description": update_bucket_item.description,
-        "done": update_bucket_item.done
-        }), 200
+                    "name": update_bucket_item.name,
+                    "description": update_bucket_item.description,
+                    "done": update_bucket_item.done
+                    }), 200
 
 
 @app.route('/bucketlists/<id>/items/<item_id>', methods=['DELETE'])
@@ -213,4 +252,4 @@ def delete_bucketlist_item():
         return jsonify({"error": "That bucketlist doesn't exist"}), 404
     db.session.delete(delete_db_item)
     db.session.commit()
-    return "Successfully deleted bucketlist item",200
+    return "Successfully deleted bucketlist item", 200
